ddcf7cff60331b3e641cab5fcf519e7c07674304
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-12-07 01:20:16 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,23 @@
-lb config \
-    --distribution trixie \
-    --binary-images iso \
+lb config -d testing \
+    --distribution "trixie" \
+    --binary-images iso-hybrid \
     --archive-areas "main contrib non-free non-free-firmware" \
+    --debootstrap-options "--variant=minbase" \
     --mirror-bootstrap "http://deb.debian.org/debian/" \
     --mirror-chroot "http://deb.debian.org/debian/" \
-    --debian-installer netinst \
-    --debian-installer-distribution trixie \
-    --debian-installer-gui false \
-    --iso-application "Debian Trixie insall" \
-    --iso-preparer "Live Build" \
-    --iso-publisher "Live Build" \
+    --bootappend-live "boot=live components username=user hostname=debian-budgie" \
     --apt-indices false \
     --apt-recommends false \
+    --security false \
+    --updates false \
+    --backports false \
     --firmware-binary true \
-    --firmware-chroot true
+    --firmware-chroot true \
+    --debian-installer live \
+    --debian-installer-distribution trixie \
+    --debian-installer-gui true \
+    --iso-application "Debian Budgie Trixie Live/Install" \
+    --iso-preparer "Live Build" \
+    --iso-publisher "Live Build"
     
 sudo lb build
