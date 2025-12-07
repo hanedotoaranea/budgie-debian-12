be25579755f393a379eb9510e7ab8199794d9520
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-12-07 17:33:17 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
     lb config -d trixie \
           --debian-installer live \
+           --iso-volume "Alice" \
           --architectures amd64 \
           --debian-installer-distribution trixie \
           --debian-installer-gui true \
           --archive-areas "main contrib non-free" \
-          --debootstrap-options "--variant=minbase" \
-          --iso-publisher "Live Build"
+          --debootstrap-options "--variant=standard" \
+          --iso-publisher "hanedotoaranea"
+          --checksums sha256 \
     
 sudo lb build
