b4aca07b63c2a4f813d258f59f884d1e9774e7bc
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-12-08 11:08:27 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-   lb config -d testing \
+   lb config  \
           --debian-installer-gui true \
-          --distribution "trixie" \
-          --binary-images iso-hybrid \
+          --distribution "bookworm" \
           --architectures amd64 \
           --binary-images iso \
           --iso-volume "Alice_$(date +%Y%m%d)" \  
@@ -11,20 +10,15 @@
           --iso-application "GitHub:https://github.com/hanedotoaranea/budgie-debian-13" \
           --checksums sha256 \
           --mirror-bootstrap http://deb.debian.org/debian \
-          --mirror-chroot http://deb.debian.org/debian \
-          --mirror-chroot-security http://security.debian.org/debian-security \
           --mirror-binary http://deb.debian.org/debian \
           --mirror-binary-security http://security.debian.org/debian-security \
           --win32-loader false \
           --apt-recommends true \
           --security true \
           --zsync true \
-          --updates true \           
-          --firmware-binary true \     
-          --firmware-chroot true \     
+          --updates true \              
           --bootloader-timeout 5 \   
           --hdd-label "ALICE_OS" \
-          --system normal \ 
           --cache false \
     
 sudo lb build
